ecfac387eab1e958ceec40088cd3b3e25da27ca9
Admin <amorozov@Jugis-2nd-MacbookPro-13.local>
2021-03-16 20:14:05 +0200

    Backup before PlayMaker

---
+++ Assets/BulletFiring.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletFiring : MonoBehaviour
+{
+    public GameObject bullet;
+    public float firerate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (bullet == null)
+        {
+            Debug.Log("Bullet is null!");
+            bullet = GameObject.FindGameObjectWithTag("bullet");
+        }
+        if (firerate == 0)
+        {
+            firerate = 2f;
+        }
+
+        StartCoroutine(Fire(bullet));
+
+    }
+
+    IEnumerator Fire(GameObject bullet)
+    {
+        while (true)
+        {
+
+            if (bullet != null)
+            {
+                bullet.transform.position = transform.position;
+                Instantiate(bullet);
+
+            }
+            yield return new WaitForSeconds(firerate);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/LevelTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Collision detected from " + collision.name);
+    }
+}
